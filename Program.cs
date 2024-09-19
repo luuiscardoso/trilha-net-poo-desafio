3bb42715fa0bcc810ccbacb559a3aeb6cff084c2
Luis Cardoso <luiscard.queiroz@gmail.com>
2024-09-19 17:01:08 -0300

    resolução

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program{
+    static void Main(string[] args)
+    {
+        Nokia nokia = new Nokia("82675813", "Modelo 1", "1234F12", 12);
+
+        Iphone iphone = new Iphone("81375814", "Modelo 1", "1234F12", 12);
+
+        nokia.Ligar();
+        nokia.InstalarAplicativo("Facebook");
+
+        iphone.Ligar();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
